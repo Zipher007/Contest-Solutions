c5df8a6ad326ad6e243cc167a1941ba9e533d5d5
Kunal Verma <72245772+Zipher007@users.noreply.github.com>
2020-10-26 12:53:14 +0530

    Create someStuff.java
    This program prints a unique star pattern for the number of row entered by the user.

---
+++ CodeStarter/Some_Stuff/someStuff.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {                // This loop runs for the i'th rows. 
+            for (int j = 1; j <= i; j++) {           // This loop will print 'i' stars corresponding to i'th row.
+                System.out.print("* ");
+            }
+            System.out.println();                // After the completion of 1 iteration, the next one will start from a new line, so used 'println'. 
+        }
+
+    }
+
+}
