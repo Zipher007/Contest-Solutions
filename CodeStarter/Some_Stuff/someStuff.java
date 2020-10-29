442f2e390eb2544f74df9dfd80dfe0a7bb7d6273
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-10-29 10:08:44 +0530

    Update someStuff.java

--- CodeStarter/Some_Stuff/someStuff.java
+++ CodeStarter/Some_Stuff/someStuff.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 public class Pattern {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {                // This loop runs for the i'th rows. 
-            for (int j = 1; j <= i; j++) {           // This loop will print 'i' stars corresponding to i'th row.
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
-            System.out.println();                // After the completion of 1 iteration, the next one will start from a new line, so used 'println'. 
+            System.out.println();
         }
-
     }
 
 }
