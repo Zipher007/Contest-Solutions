ce104059398eb037bd8c0ebed0c22c73c21942eb
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-10-29 10:27:36 +0530

    Update superstitions.cpp

--- CodeStarter/Superstitions/superstitions.cpp
+++ CodeStarter/Superstitions/superstitions.cpp
@@ -7,28 +7,24 @@ int main()
     cin>>n;
     int x;
     cin>>x;
-    
-   vector<int>arr(n);
-   for(int i=0;i<n;i++)
-   {
-       cin>>arr[i];
-   }
-   
-   set<int>m;
-   int res=0;
-   for(int i=0;i<n;i++)
-   {
-       if(m.find(x-arr[i])!=m.end())
-       {
-           res++;
-       }
-       else
-       {
-           m.insert(arr[i]);
-       }
-   }
+    vector<int>arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    set<int>m;
+    int res=0;
+    for(int i=0;i<n;i++)
+    {
+        if(m.find(x-arr[i])!=m.end())
+        {
+            res++;
+        }
+        else
+        {
+            m.insert(arr[i]);
+        }
+    }
    cout<<res<<endl;
-   
-	return 0;
-	
+   return 0;
 }
